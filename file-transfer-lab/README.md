2a672bdfb17df622c13f3f6c6d519e528ccf6fe2
Icey <isaigonz48@gmail.com>
2018-10-02 14:39:05 -0600

    Added multiple client functionality

--- file-transfer-lab/README.md
+++ file-transfer-lab/README.md
@@ -1,11 +1,11 @@
 Sending/Receiving Files Lab
 
-* fileClient.py will ask for the name of the file that you want to send
+* fileClient.py will ask user for input of the name of the file that you want to send
 
 * fileServer.py will receive the file and put its contents into a new file called "RECEIVE[sent file name]"
 ** This was done to avoid confusion in what file was sent and what file was received
 
-* fileServer.py still only supports one client
+* fileServer.py supports multiple clients
 
 * fileClient.py and fileServer.py use  framedSock.py to frame the messages
 
--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import sys
 sys.path.append("../lib")       # for params
-import re, socket, params
+import re, socket, params, os
 
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
@@ -23,46 +23,42 @@ lsock.bind(bindAddr)
 lsock.listen(5)
 print("listening on:", bindAddr)
 
-sock, addr = lsock.accept()
-
-print("connection rec'd from", addr)
-
-
-from framedSock import framedSend, framedReceive
-totalFile = ""
-newLineCounter = 0
-fileName = framedReceive(sock, debug)
-
 while True:
-    
-    payload = framedReceive(sock, debug)
-    if newLineCounter == 1:
-        if debug: print("rec'd: ", payload)
-        #if not payload:
-            #totalFile = totalFile[:-1]#"\n"
-        #    if makeNewLine == 1:
-        #        totalFile += '\n'
-        #        newLineCounter = 0
-        #        continue
-        #    break
-        totalFile += payload.decode()# + '\n'
-        if makeNewLine == 1:
-            totalFile += '\n'
+    sock, addr = lsock.accept()
+
+    #print("connection rec'd from", addr)
+
+
+    from framedSock import framedSend, framedReceive
+
+    if not os.fork():
+        print("child handling stuff form")
+
+        totalFile = ""
         newLineCounter = 0
-    else:
-        if not payload:
-            #totalFile = totalFile[:-1]#"\n"
-            break
-        if payload.decode() == "YES":
-            makeNewLine = 1
-        newLineCounter = 1
-        #print ("payload: %s" % payload.decode())             # make emphatic!
-        #framedSend(sock, payload, debug)
-#print (totalFile)
+        fileName = framedReceive(sock, debug)
 
-file = open(("RECEIVED" + fileName.decode()), 'w')
+        while True:
+    
+            payload = framedReceive(sock, debug)
+            if newLineCounter == 1:
+                if debug: print("rec'd: ", payload)
+                totalFile += payload.decode()
+                if makeNewLine == 1:
+                    totalFile += '\n'
+                    newLineCounter = 0
+            else:
+                if not payload:
+                    break
+                if payload.decode() == "YES":
+                    makeNewLine = 1
+                newLineCounter = 1
 
-file.write(totalFile)
+        if not fileName:
+            sys.exit()
+        file = open(("RECEIVED" + fileName.decode()), 'w')
 
-file.close()
+        file.write(totalFile)
+        
+        file.close()
 
